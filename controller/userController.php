c9b5db4ac577e74d2aaf854c7e5dce7032ab713f
belarif <b.ocine@live.fr>
2021-11-04 14:17:59 +0100

    delete unnecessary files

--- controller/userController.php
+++ controller/userController.php
@@ -1,29 +1,3 @@
 <?php
 
-require_once 'model/UserManager.php';
-
-
-function listUsersAction($template)
-{
-    $UserManager = new Hocine\Blog\Model\UserManager();
-    $posts = $UserManager->getUsers();
-
-    echo $template->render(['f' => 'g']);
-
-}
-
-function addUserAction($template)
-{
-    echo $template->render(['f' => 'k']);
-}
-
-function editUserAction($template)
-{
-    echo $template->render(['f' => 'l']);
-}
-
-function readUserAction($template)
-{
-    echo $template->render(['g' => 'e']);
-}
 
