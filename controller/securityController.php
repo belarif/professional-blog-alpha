cf9e6080d8f854d8c9ba1eb8da4cda36da328af3
belarif <b.ocine@live.fr>
2021-11-05 12:08:49 +0100

    delete unnecessary content and files

--- controller/securityController.php
+++ controller/securityController.php
@@ -1,18 +1,1 @@
 <?php
-
-require('model/SecurityManager.php');
-
-function dashboardAction($template)
-{
-    echo $template->render(['d' => 'jh']);
-}
-
-function loginAction($template)
-{
-    echo $template->render(['a' => 'b']);
-}
-
-function registerAction($template)
-{
-    echo $template->render(['a' => 'b']);
-}
--- model/HomeManager.php
+++ model/HomeManager.php
@@ -1,11 +1,1 @@
 <?php
-
-function cv()
-{
-    echo 'cv non disponible encore';
-}
-
-function socialNetwork()
-{
-    echo 'pas encore disponible';
-}
