180ac65d5f7c7ad06616a4e503b9a39c00278ee7
belarif <b.ocine@live.fr>
2021-11-10 09:09:31 +0100

    just display of the home page with -render()-

--- controller/homeController.php
+++ controller/homeController.php
@@ -1,3 +1,22 @@
 <?php
 
+namespace ProfessionalBlog\Controller;
+
+use ProfessionalBlog\Model\HomeManager;
+
+require_once 'model/HomeManager.php';
+
+class HomeController
+{
+    public function homeAction($template)
+    {
+        echo $template->render(['f' => 'f']);
+    }
+
+    public function sendMessageAction()
+    {
+
+    }
+}
+
 
--- model/HomeManager.php
+++ model/HomeManager.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace ProfessionalBlog\Model;
+
+use ProfessionalBlog\Model\Manager;
+
+require_once 'model/Manager.php';
+
+class HomeManager extends Manager
+{
+
+}
