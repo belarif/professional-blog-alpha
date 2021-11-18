776a7bfa19307586b82bbc541ee9eabcba06c527
belarif <b.ocine@live.fr>
2021-11-18 22:10:57 +0100

    merging contact branch in the main branch

--- controller/homeController.php
+++ controller/homeController.php
@@ -2,17 +2,76 @@
 
 namespace ProfessionalBlog\Controller;
 
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\PHPMailer;
+
+require 'vendor/autoload.php';
+
 class HomeController
 {
     public function homeAction($template)
     {
         session_start();
         $logged_user = $_SESSION['logged_user'];
-
         echo $template->render(['logged_user' => $logged_user]);
+    }
 
+    public function sendMessage()
+    {
+        try {
+            if(isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message']))
+            {
+                if(!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message']))
+                {
+                    if(isset($_POST['submit']))
+                    {
+                        $firstName = $_POST['firstName'];
+                        $lastName = $_POST['lastName'];
+                        $message = $_POST['message'];
+                        $email = $_POST['email'];
+                        $subject = $_POST['subject'];
+                        $content = "<p><b>Nom : </b>".$lastName."</p>
+                            <p><b>Pr&eacute;nom : </b>".$firstName."</p>
+                            <p><b>Message : </b>".$message."</p>
+                            <p><b>E-mail : </b>".$email."</p>
+                            ";
+
+                        $mail = new PHPMailer(true);
+
+                        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                        $mail->isSMTP();
+                        $mail->Host       = 'smtp.gmail.com';
+                        $mail->SMTPAuth   = true;
+                        $mail->Username   = 'belarif.test@gmail.com';
+                        $mail->Password   = 'EmailTest';
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                        $mail->Port       = 465;        //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                        $mail->setFrom($email,$lastName);
+                        $mail->addAddress('belarif.test@gmail.com');
+                        $mail->isHTML(true);
+                        $mail->Subject = $subject;
+                        $mail->Body    = $content;
+
+                        if ($mail->send())
+                        {
+                            header("location:index.php?action=home");
+                        }
+                        else{
+                            echo 'error';
+                        }
+                    }
+                }
+                else
+                {
+                    throw new \Exception("Veuillez renseigner tous les champs");
+                }
+            }
+        }
+        catch (\Exception $e)
+        {
+            $errorMessage = $e->getMessage();
+            /*echo $template->render(['errorMessage' => $errorMessage]);*/
+        }
     }
 
 }
-
-
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,8 +7,8 @@ namespace Composer\Autoload;
 class ComposerStaticInitefe9bd41851fa8d0af317d27bb5eee9a
 {
     public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -20,6 +20,10 @@ class ComposerStaticInitefe9bd41851fa8d0af317d27bb5eee9a
         array (
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
+        ),
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
     );
 
@@ -35,6 +39,10 @@ class ComposerStaticInitefe9bd41851fa8d0af317d27bb5eee9a
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
     );
 
