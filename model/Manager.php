e1fe690a7fc14f9b13a1bc3cdb50024fdd61a812
belarif <b.ocine@live.fr>
2021-12-08 16:16:00 +0100

    additing variable BD_HOST

--- model/Manager.php
+++ model/Manager.php
@@ -8,7 +8,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=localhost;dbname='.$_ENV['DATABASE'].';charset=utf8',$_ENV['USERNAME'],$_ENV['PASSWORD'],
+        $db = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname='.$_ENV['DATABASE'].';charset=utf8',$_ENV['USERNAME'],$_ENV['PASSWORD'],
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
         return $db;
     }
