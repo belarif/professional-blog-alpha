8371a179b02873181e6f060c5ef50a86b2042acb
belarif <b.ocine@live.fr>
2021-12-08 18:31:48 +0100

    delete unancessary variable

--- model/Manager.php
+++ model/Manager.php
@@ -8,8 +8,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DATABASE'] . ';charset=utf8', $_ENV['USERNAME'], $_ENV['PASSWORD'],
+        return new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DATABASE'] . ';charset=utf8', $_ENV['USERNAME'], $_ENV['PASSWORD'],
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        return $db;
     }
 }
