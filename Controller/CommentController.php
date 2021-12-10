95444fb87abad6a57d97cdb5ec51faad7262b0df
belarif <b.ocine@live.fr>
2021-12-10 19:44:41 +0100

    protection of forms for modifying comments against the CSRF flaw

--- Controller/CommentController.php
+++ Controller/CommentController.php
@@ -13,11 +13,12 @@ class CommentController
     public function listComments($template)
     {
         session_start();
-        if (isset($_SESSION['logged_user']) && $_SESSION['role'] == 1) {
+        if (isset($_SESSION['logged_user']) && isset($_SESSION['token']) && $_SESSION['role'] == 1) {
             $logged_user = $_SESSION['logged_user'];
+            $token = $_SESSION['token'];
             $commentManager = new CommentManager();
             $listComments = $commentManager->getComments();
-            echo $template->render(['listComments' => $listComments, 'logged_user' => $logged_user]);
+            echo $template->render(['listComments' => $listComments, 'logged_user' => $logged_user, 'token' => $token]);
         } else {
             header("Location:index.php?action=login");
         }
@@ -29,14 +30,15 @@ class CommentController
     public function readComment($template)
     {
         session_start();
-        if (isset($_SESSION['logged_user']) && $_SESSION['role'] == 1) {
+        if (isset($_SESSION['logged_user']) && isset($_SESSION['token']) && $_SESSION['role'] == 1) {
             $logged_user = $_SESSION['logged_user'];
+            $token = $_SESSION['token'];
             if (isset($_GET['id'])) {
                 $id = $_GET['id'];
                 $commentManager = new CommentManager();
                 $comment = $commentManager->getComment($id);
                 if ($comment) {
-                    echo $template->render(['comment' => $comment, 'logged_user' => $logged_user]);
+                    echo $template->render(['comment' => $comment, 'logged_user' => $logged_user, 'token' => $token]);
                 } else {
                     header('Location:index.php?action=non-existent-backoffice-page');
                 }
@@ -52,21 +54,23 @@ class CommentController
     public function editComment($template)
     {
         session_start();
-        if (isset($_SESSION['logged_user']) && $_SESSION['role'] == 1) {
-            $logged_user = $_SESSION['logged_user'];
-            if (isset($_GET['id'])) {
+        if (isset($_SESSION['logged_user']) && isset($_SESSION['token']) && $_SESSION['role'] == 1) {
+            if (isset($_GET['token']) && $_GET['token'] == $_SESSION['token']) {
+                $logged_user = $_SESSION['logged_user'];
+                $token = $_SESSION['token'];
                 $id = $_GET['id'];
                 $commentManager = new CommentManager();
                 $comment = $commentManager->getComment($id);
                 if ($comment) {
-                    echo $template->render(['comment' => $comment, 'logged_user' => $logged_user]);
+                    echo $template->render(['comment' => $comment, 'logged_user' => $logged_user, 'token' => $token]);
                 } else {
                     header('Location:index.php?action=non-existent-backoffice-page');
                 }
-
             } else {
-                header("Location:index.php?action=login");
+                header("Location:index.php?action=non-existent-backoffice-page");
             }
+        } else {
+            header("Location:index.php?action=login");
         }
     }
 
@@ -97,16 +101,16 @@ class CommentController
                 $id = $_GET['id'];
                 $commentManager = new CommentManager();
                 $comment = $commentManager->getComment($id);
-                if($comment){
+                if ($comment) {
                     $commentManager->deleteComment($id);
                     header("Location:index.php?action=dashboard/listComments");
-                }else {
+                } else {
                     header("Location:index.php?action=non-existent-backoffice-page");
                 }
-            }else {
+            } else {
                 header("Location:index.php?action=non-existent-backoffice-page");
             }
-        }else {
+        } else {
             header("Location:index.php?action=login");
         }
     }
