955a26dae91815e57de9c1b84d0527a5b50ef8e5
belarif <b.ocine@live.fr>
2021-12-13 13:52:36 +0100

    application of the SRP (Single Responsibility principle) to the login function

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -11,60 +11,15 @@ class SecurityController
     /**
      * @param $template
      */
-    public function login(TemplateWrapper $template)
+    public function loginForm(TemplateWrapper $template)
     {
         try {
             session_start();
-            if (isset($_POST['email']) && isset($_POST['password'])) {
-                if (!empty($_POST['email']) && !empty($_POST['password'])) {
-                    $email = strip_tags($_POST['email']);
-                    $password = strip_tags($_POST['password']);
-
-                    if (isset($_POST['submit'])) {
-                        $UserManager = new UserManager();
-                        $user = $UserManager->getLoginUser($email);
-
-                        if ($user) {
-                            $hashPassword = $user['password'];
-                            if (password_verify($password, $hashPassword)) {
-                                $_SESSION['logged_user'] = $user['lastName'];
-                                $_SESSION['role'] = $user['role'];
-                                $_SESSION['id'] = $user['id'];
-                                if (!isset($_SESSION['token'])) {
-                                    $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(20));
-                                }
-
-                                if (isset($_SESSION['logged_user'])) {
-                                    $token = $_SESSION['token'];
-                                    if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
-                                        header("Location:index.php?action=dashboard");
-                                    } else {
-                                        if (isset($_SESSION['current_post_id']) && $_SESSION['current_post_id'] !== null) {
-                                            $post_id = $_SESSION['current_post_id'];
-                                            $_SESSION['current_post_id'] = null;
-                                            header("Location:index.php?action=commentPost&id=$post_id&token=$token");
-                                        } else {
-                                            header("Location:index.php?action=listPosts");
-                                        }
-                                    }
-                                } else {
-                                    header("Location:index.php?action=login");
-                                }
-                            } else {
-                                throw new Exception("Mot de passe invalide !!");
-                            }
-                        } else {
-                            throw new Exception("Aucun compte existant avec l'adresse : $email");
-                        }
-                    }
-                } else {
-                    throw new Exception("Veuillez renseigner votre email et/ou mot de passe");
-                }
-            }
-
+            $this->login();
             if (isset($_SESSION['successRegister']) && $_SESSION['successRegister'] !== null) {
                 $successRegister = $_SESSION['successRegister'];
                 $_SESSION['successRegister'] = null;
+
                 echo $template->render(['successRegister' => $successRegister]);
             } else {
                 echo $template->render();
@@ -72,6 +27,76 @@ class SecurityController
         } catch (Exception $e) {
             $identificationError = $e->getMessage();
             echo $template->render(['identificationError' => $identificationError]);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function login()
+    {
+        if (isset($_POST['email']) && isset($_POST['password'])) {
+            if (!empty($_POST['email']) && !empty($_POST['password'])) {
+                if (isset($_POST['submit'])) {
+                    $email = strip_tags($_POST['email']);
+                    $password = strip_tags($_POST['password']);
+
+                    $this->verifyUser($email,$password);
+
+                } else {
+                    throw new Exception("Veuillez renseigner votre email et/ou mot de passe");
+                }
+            }else {
+                throw new Exception("tous les champs sont obligatoires");
+            }
+        }
+    }
+
+    /**
+     * @param $email
+     * @param $password
+     * @throws Exception
+     */
+    private function verifyUser($email, $password)
+    {
+        $UserManager = new UserManager();
+        $user = $UserManager->getLoginUser($email);
+        if ($user) {
+            $hashPassword = $user['password'];
+            if (password_verify($password, $hashPassword)) {
+                $_SESSION['logged_user'] = $user['lastName'];
+                $_SESSION['role'] = $user['role'];
+                $_SESSION['id'] = $user['id'];
+
+                $this->redirectUser();
+            } else {
+                throw new Exception("Mot de passe invalide !!");
+            }
+        } else {
+            throw new Exception("Aucun compte existant avec l'adresse : $email");
+        }
+    }
+
+    private function redirectUser()
+    {
+        if (isset($_SESSION['logged_user'])) {
+            if (!isset($_SESSION['token'])) {
+                $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(20));
+            }
+            $token = $_SESSION['token'];
+            if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
+                header("Location:index.php?action=dashboard");
+            } else {
+                if (isset($_SESSION['current_post_id']) && $_SESSION['current_post_id'] !== null) {
+                    $post_id = $_SESSION['current_post_id'];
+                    $_SESSION['current_post_id'] = null;
+                    header("Location:index.php?action=commentPost&id=$post_id&token=$token");
+                } else {
+                    header("Location:index.php?action=listPosts");
+                }
+            }
+        } else {
+            header("Location:index.php?action=login");
         }
     }
 
@@ -121,7 +146,7 @@ class SecurityController
     public function dashboard(TemplateWrapper $template)
     {
         session_start();
-        if (isset($_SESSION['logged_user']) && $_SESSION['role'] == 1) {
+        if (isset($_SESSION['logged_user']) && isset($_SESSION['role']) && $_SESSION['role'] == 1) {
             $logged_user = $_SESSION['logged_user'];
             $token = $_SESSION['token'];
             echo $template->render(['logged_user' => $logged_user, 'token' => $token]);
